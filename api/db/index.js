9267b13e9cee2b8dcefd3cb4a98be35245086e01
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-03-23 11:13:24 +0000

    Update logging setting to false in db index file

--- api/db/index.js
+++ api/db/index.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT,
-    loggin: false
+    logging: false
 })
 
 module.exports = sequelize
